789193c40d0716cd0e313b5b914bc33caff5a733
bofoto <cho03k@gmail.com>
2025-07-09 13:16:44 +0900

    add simple code 	modified:   lib/main.dart 	new file:   lib/screens/MainScreen.dart 	new file:   lib/screens/SplashScreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,53 @@
+import 'package:onpoint/screens/SplashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  //runApp(const MyApp());
+  runApp(OnPointApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+/*class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      //home: Text('dkssud')
+      //home: Image.asset('croco.jpg')
+      //home: SizedBox --> Conteiner
+      //home: Container( width: 50, height: 50, color: Colors.greenAccent /*.fromARGB(255, 10, 165, 255)*/)
+      // home: Center(
+      //   child: Container( width:50, height: 50, color: const Color.fromARGB(255, 209, 14, 14),
+      //     )        
+      //   )
+    /******************************************************/
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: Text('앱임')),
+        
+        body: Text(
+          '안녕'
         ),
-      ),
+        bottomNavigationBar: BottomAppBar( child: SizedBox(
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message , color: Colors.blueAccent),
+              Icon(Icons.contact_page),
+              ],),
+        ),),
+      )
+
     );
   }
 }
+*/
+class OnPointApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: '온점',
+      home: SplashScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/MainScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(  // 화면의 뼈대
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("온점", style: TextStyle(fontSize: 32)),
+      ),
+    );
+  }
+}
